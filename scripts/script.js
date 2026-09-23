490528cc4945397c29df09a0ed88a53030c61f04
schindlerwork <schindler@xtendly.com>
2026-09-23 16:57:54 +0800

    Add: Navigation Bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelector(".nav");
+
+
+openBtn.addEventListener("click", () => {
+  nav.classList.add("nav-show");
+})
+
+closeBtn.addEventListener("click", () => {
+  nav.classList.remove("nav-show");
+})
